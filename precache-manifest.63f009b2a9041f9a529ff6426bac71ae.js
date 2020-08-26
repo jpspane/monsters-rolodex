61b027ce58fdb77c338b2887288a9a61e0f68539
jspane2000 <jpspane@gmail.com>
2020-08-26 14:44:41 -0700

    Updates

---
+++ precache-manifest.63f009b2a9041f9a529ff6426bac71ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d745100d4e5a7bc36f896efbe6192c69",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d425a46f6d4b1e9f83af",
+    "url": "/monsters-rolodex/static/css/main.466b2878.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d425a46f6d4b1e9f83af",
+    "url": "/monsters-rolodex/static/js/main.1b885c14.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
